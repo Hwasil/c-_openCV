fb9705a22fba1aefc46c3e53079b2ecdacadc910
Hwasil <127382144+Hwasil@users.noreply.github.com>
2023-12-15 19:32:54 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 int x;
 // int x
 
-/*
+
 for (int i = 1; i < 31; i = i + 2) {			// 홀수값
 	GaussianBlur(src, dst, Size(i, i), 0, 0);	// 가우시안 스무딩
 	imshow("Gaussian filter", dst);
 	waitKey(1000);
-} */
+} 
